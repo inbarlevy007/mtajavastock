929479ae123dc9835083a0da3aa579664799caf0
Inbar Levy <inbarlevy007@gmail.com>
2014-11-15 18:15:54 +0200

    Exercise 3

--- src/com/mta/javacourse/MTA_InbarServlet.java
+++ src/com/mta/javacourse/MTA_InbarServlet.java
@@ -13,7 +13,36 @@ public class MTA_InbarServlet extends HttpServlet {
 		int num3 = 7;
 		int res = (num1+num2)*num3;
 		
-		String resultStr = new String ("<h1>The result of : " + "(" + num1 + "+" + num2 + ")" + "*" + num3 + " = " + res + "</h1>" );
+		String resStr = new String ("<h1>The result of : " + "(" + num1 + "+" + num2 + ")" + "*" + num3 + " = " + res + "</h1>" );
+		
+		//circle area
+		int radius = 50;
+		double circleArea = (radius * radius) * Math.PI;
+		String roundCircleArea = String.format("%.2f", circleArea);
+		
+		String line1 = new String ("calculation 1: Area of circle with radius " + radius + " is: " + roundCircleArea + " square-cm.");
+		
+		
+		//opposite of triangle
+		int bAngle = 30;
+		int hypotenuse = 50;
+		
+		double degreeToRadian = Math.toRadians(bAngle);
+		double opposite = hypotenuse * degreeToRadian;
+		String roundOpposite = String.format("%.2f", opposite);
+		String line2 = new String ("calculation 2: Length of opposite, where angle B is: " + bAngle + " is: " + roundOpposite + ".");
+		
+		//calculate power of 20
+		int base = 20;
+		int exp = 13;
+		long powResult = 1;
+		
+		for (int i = 1; i <= exp; i++)
+			powResult *= base;
+		
+		String line3 = new String ("calculation 3: Power of " + base +" with exp of " + exp + " is: " + powResult);
+		
+		String resultStr = resStr + "<br>" + line1 + "<br>" + line2 + "<br>" + line3;
 		
 		resp.setContentType("text/html");
 		resp.getWriter().println(resultStr);
