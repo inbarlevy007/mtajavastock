5a8f3dc41df9c0b67355ddc809daa5f11b1e12cf
Inbar Levy <inbarlevy007@gmail.com>
2014-11-24 13:05:00 +0200

    Exercise 4

---
+++ src/com/mta/javacourse/Stock.java
@@ -0,0 +1,49 @@
+package com.mta.javacourse;
+
+import java.util.Date;
+ 
+public class Stock {
+       
+        //Data Members
+        private String Symbol;
+        private float Ask;
+        private float Bid;
+        private Date Date;
+       
+        //Setters and Getters--->
+       
+        public String getSymbol() {
+                return Symbol;
+        }
+        public void setSymbol(String symbol) {
+                Symbol = symbol;
+        }
+       
+        public float getAsk() {
+                return Ask;
+        }
+        public void setAsk(float ask) {
+                Ask = ask;
+        }
+       
+        public float getBid() {
+                return Bid;
+        }
+        public void setBid(float bid) {
+                Bid = bid;
+        }
+     
+        public java.util.Date getDate() {
+                return Date;
+        }
+        public void setDate(java.util.Date date) {
+                Date = date;
+        }
+       
+        public String getHtmlDescription()
+        {
+                String stockHtmlDetailsString = "<b>Stock symbol</b>: " +getSymbol() + "<b> Stock Ask</b>: " +getAsk() + "<b> Bid</b>: " +getBid() + "<b> Stock Date</b>: " +getDate();
+                return stockHtmlDetailsString;
+        }
+ 
+}
